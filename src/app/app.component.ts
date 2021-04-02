05b826a7f17b1fd94432422111e82f2ee19a6e0f
BHARATH GORENTAL <bharathgorental@gmail.com>
2021-04-02 11:47:18 +0530

    Angular_Template driven form final commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,14 +11,45 @@ export class AppComponent {
   defaultQuesAns = 'pet';
   defaultEmail = "Please Enter your e-mail";
   answer = "";
-
   genders = ['male', 'female'];
+
+  submitted = false;
+  userDetails = {
+    username:'',
+    email:'',
+    secret:'',
+    answer:'',
+    gender:''
+  }
 
   suggestUserName() {
     const suggestedName = 'Superuser';
+
+    // this.signUpForm.setValue({
+    //   userData   : {
+    //     username : suggestedName,
+    //     email    : '',
+    //   },
+    //   secret     : 'pet',
+    //   questAns   : '',
+    //   gender     : 'male'
+    // })
+
+    this.signUpForm.form.patchValue({
+      userData:{
+        username:suggestedName
+      }
+    })
   }
 
   onSubmitted(){
-    console.log(this.signUpForm);
+    // console.log(this.signUpForm);
+    this.submitted = true;
+    this.userDetails.username = this.signUpForm.value.userData.username;
+    this.userDetails.email = this.signUpForm.value.userData.email;
+    this.userDetails.secret = this.signUpForm.value.secret;
+    this.userDetails.answer = this.signUpForm.value.questAns;
+    this.userDetails.gender = this.signUpForm.value.gender;
+    this.signUpForm.reset();
   }
 }
